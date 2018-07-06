4bdb4ac3ed596aa1ed6b8b0ff235140eb9042d94
stlefebvre <lefebvre.shelby@gmail.com>
2018-07-05 23:05:49 -0400

    added comments for guidance, updated PUT and POST methods, need to add GET method

--- views/js/inventory.js
+++ views/js/inventory.js
@@ -1,60 +1,72 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-sleep").on("click", function(event) {
+// Use PUT method to update information with both "rent" and "donate" for items.
+$(function () {
+  $("#submit").on("click", function (event) {
+    //if this doesn't work, try "if rent is checked" statement
+    if ($("#rent") === "renter") {
       var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-  
-      var newSleepState = {
-        sleepy: newSleep
+      var rentItem = $(this).data("rentItem");
+      //create variable in form to select quantity
+
+      var rentItem = {
+        quantity: quantity
       };
-  
+
       // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
+      $.ajax("/api/paknplay/" + id, {
         type: "PUT",
-        data: newSleepState
+        data: rentItem
       }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
+        function () {
+          console.log(quantity + "of " + id + " successfully rented");
           // Reload the page to get the updated list
           location.reload();
         }
       );
-    });
-  
-    $(".create-form").on("submit", function(event) {
+    }
+  });
+
+  //Adding new item for a donator
+  $("#submit").on("submit", function (event) {
+    //If rent=renter doesn't work, try making an "if renter is checked" statement
+    //Is there a way to check stock item in case someone is donating an item that already exists? This will determine if item needs a PUT or POST method
+    if ($("#donate") === "donator") {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+
+      var newitem = {
+        name: $("#dItemName").val(),
+        category: $("#dItemCtgy").val()
+        //add qty field
       };
-  
+
       // Send the POST request.
-      $.ajax("/api/cats", {
+      $.ajax("/api/paknplay", {
         type: "POST",
-        data: newCat
+        data: newitem
       }).then(
-        function() {
-          console.log("created new cat");
+        function () {
+          console.log("added new item");
           // Reload the page to get the updated list
           location.reload();
         }
       );
-    });
-  
-    $(".delete-cat").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted cat", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+    }
   });
+
+  //Commenting delete method out on lines 53-66 for the time being, Not sure if we will need to use this at all. Even if stock item is down to zero, we simply would not show it to the user on the front end. No need to delete in case someone donates an item, thus replenishing the stock.
+
+  // $(".delete-cat").on("click", function (event) {
+  //   var id = $(this).data("id");
+
+  //   // Send the DELETE request.
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function () {
+  //       console.log("deleted cat", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+});
